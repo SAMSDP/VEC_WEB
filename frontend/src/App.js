96c2b04a417794b58caa94edc3c576ffb5dc2b75
SAMSDP <samsdpaul150@gmail.com>
2025-01-17 10:48:18 +0530

    With Home Page
    Testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 import NavigationBar from './Components/NavigationBar';
 import Footer from './Components/Footer';
 import LandingPage from './Pages/LandingPage';
+import Home from './Pages/Home';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <div style={{ minHeight: '80vh' }}>
           <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
           </Routes>
         </div>
         <Footer />
